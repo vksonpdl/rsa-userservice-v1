f94bd7e923d2444ea649be47a72fc04ab8144395
Vikas Patoli <vksonpdl@gmail.com>
2021-12-24 12:21:24 +0530

    Enabed CrossOrigin

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 
 @RestController
+@CrossOrigin (origins = {"http://localhost:4200","http://146.148.98.149","http://146.148.98.149:80"})
 @RequestMapping("/user")
 public class UserController {
 
